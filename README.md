c79a607d8e6d2380f170f47ba5523bb45ae9b0a4
zengkai <shzengkai@corp.netease.com>
2018-09-06 18:40:26 +0800

    throttle

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# underscore
+#underscore
 跟着这个[BLOG](https://github.com/mqyqingfeng/Blog)
 
 ##不清楚的地方：  
@@ -6,9 +6,9 @@ instanceof 看了 [这篇](https://www.cnblogs.com/Trr-984688199/p/6180040.html)
 _.mixin 看了 [这篇](https://blog.csdn.net/u012468376/article/details/53121081)  
 call apply 看了 [这篇](https://www.cnblogs.com/zhumingzhenhao/p/8337146.html)  
 isArrayLike 看了 [这篇](https://www.cnblogs.com/wjx91/p/5798598.html)  
-reutn function(x){ return x } 柯里函数 看了 [这篇](https://www.zhangxinxu.com/wordpress/2013/02/js-currying/)  
+reutn function(x){ return x } 柯里函数 看了 [这篇](https://www.zhangxinxu.com/wordpress/2013/02/js-currying/)
 
-#debounce
+#debounce  
 跟着这个[BLOG](https://github.com/mqyqingfeng/Blog/issues/58)
 
 ##不清楚的地方：
--- debounce/index.js
+++ debounce/index.js
@@ -1,17 +1,78 @@
 var count = 1;
 var container = document.getElementById('container');
 
-function getUserAction() {
+function getUserAction(e) {
     container.innerHTML = count++;
 };
 
-function debounce(func, wait){
-	var timeout;
-	return function(){
-		clearTimeout(timeout);
-		timeout = setTimeout(func, wait);
-	}
+/*function debounce(func, wait) {
+    var timeout;
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(func, wait);
+    }
+}*/
 
+function debounce(func, wait) {
+    var timeout;
+
+    return function () {
+        var context = this;
+        var args = arguments;
+
+        clearTimeout(timeout)
+        timeout = setTimeout(function(){
+            func.apply(context, args)
+        }, wait);
+    }
 }
 
-container.onmousemove = debounce(getUserAction, 1000);
+/*function debounce(func, wait, immediate){
+	var timeout;
+	return function(){
+		var context = this;
+		var args = arguments;
+
+		if (timeout) clearTimeout(timeout);
+		if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null;
+            }, wait)
+            if (callNow) func.apply(context, args)
+        }else {
+            timeout = setTimeout(function(){
+                func.apply(context, args)
+            }, wait);
+        }
+	}
+}*/
+
+/*function debounce(func, wait, immediate) {
+
+    var timeout, result;
+
+    return function () {
+        var context = this;
+        var args = arguments;
+
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null;
+            }, wait)
+            if (callNow) result = func.apply(context, args)
+        }
+        else {
+            timeout = setTimeout(function(){
+                func.apply(context, args)
+            }, wait);
+        }
+        return result;
+    }
+}*/
+
+container.onmousemove = debounce(getUserAction, 1000, true);
